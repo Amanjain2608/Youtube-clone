9468163972b224434b9ec5f0b45cfc37aade6ba5
Aman Jain <jain.aman2608@gmail.com>
2023-08-30 15:20:04 +0530

    new commit

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -5,11 +5,13 @@ import LeftNav from "./LeftNav";
 import videoCard from "./VideoCard";
 
 const Feed = () => {
-  const { loading, searchResult } = useContext(Context);
+  const [loading, searchResults] = useContext(Context);
 
   useEffect(()=>{
     document.getElementById("root").classList.remove("custom-h");
-  },[])
+  },[]);
+
+  console.log(searchResults)
 
   return (
     <div className="flex flex-row h-[calc(100%-44px)]">
@@ -17,8 +19,8 @@ const Feed = () => {
       <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
           {!loading &&
-            searchResult.map((item) => {
-              if(item.type !=="video") return "";
+            searchResults.map((item) => {
+              if(item.type !=="video") return false;
               return (
                 <videoCard key={item?.video?.videoId} video={item?.video} />
               );
