90ebd4dda87f6244a78d6067f8cd31a88f36bd6d
Aman Jain <jain.aman2608@gmail.com>
2023-08-30 15:41:16 +0530

    new commit

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -5,12 +5,13 @@ import LeftNav from "./LeftNav";
 import videoCard from "./VideoCard";
 
 const Feed = () => {
-  const [loading, searchResults] = useContext(Context);
+  const {loading, searchResults} = useContext(Context);
 
   useEffect(()=>{
     document.getElementById("root").classList.remove("custom-h");
   },[]);
 
+  console.log(typeof(searchResults))
   console.log(searchResults)
 
   return (
@@ -20,7 +21,7 @@ const Feed = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
           {!loading &&
             searchResults.map((item) => {
-              if(item.type !=="video") return false;
+              if(item.type !=="video") return " ";
               return (
                 <videoCard key={item?.video?.videoId} video={item?.video} />
               );
